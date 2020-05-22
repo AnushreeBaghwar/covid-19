cfd93a4721ce24abc1cb9c3ba8bcad407f2a23a9
Anushree Baghwar <anushreebaghwar03@gmail.com>
2020-05-22 16:07:28 +0530

    added "try"

--- covid.py
+++ covid.py
@@ -32,7 +32,10 @@ def all():
 			c.append(td.text)
 		r = requests.get('https://restcountries.eu/rest/v2/name/'+c[1]+'?fullText=true')
 		r_data = r.json()
-		c[0] = r_data['flag']
+		try:
+			c[0] = r_data['flag']
+		except Exception:
+			pass
 		countries_data.append(c)
 	return render_template('all.html',data=countries_data,l=len(countries_data))
 
