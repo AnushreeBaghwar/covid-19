cb0aa6a4cf7cbe258eb182dcd6a4b48174671b07
Anushree Baghwar <anushreebaghwar03@gmail.com>
2020-05-22 15:22:28 +0530

    added function

--- covid.py
+++ covid.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from bs4 import BeautifulSoup as bs
 import requests
 app=Flask(__name__)
 @app.route('/')
@@ -15,6 +16,22 @@ def reslt():
 	res=requests.get(url)
 	return render_template('result.html',data=res.json()['cases_time_series'],l=len(res.json()['cases_time_series']))
 
+
+@app.route('/all')
+def all():
+	url="https://worldometers.info/coronavirus"
+	res=requests.get(url)
+	soup=bs(res.text)
+	t_body_list=soup.select('tbody')
+	t_body = t_body_list[0]
+	tr_list=t_body.select('tr')
+	countries_data=[]
+	for country in tr_list:
+		c=[]
+		for td in country.select('td'):
+			c.append(td.text)
+		countries_data.append(c)
+	return render_template('all.html',data=countries_data,l=len(countries_data))
 
 
 
