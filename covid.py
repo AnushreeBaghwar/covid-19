39e40de73203ff1192bb3ec7d0c9b94421588b32
Anushree Baghwar <anushreebaghwar03@gmail.com>
2020-05-22 20:18:49 +0530

    reduced max api requestss

--- covid.py
+++ covid.py
@@ -2,7 +2,29 @@ from flask import Flask,render_template
 from bs4 import BeautifulSoup as bs
 import requests
 app=Flask(__name__)
-@app.route('/')
+
+url="https://worldometers.info/coronavirus"
+res=requests.get(url)
+soup=bs(res.text)
+t_body_list=soup.select('tbody')
+t_body = t_body_list[0]
+tr_list=t_body.select('tr')
+countries_data=[]
+flags = []
+for country in tr_list:
+    c=[]
+    for td in country.select('td'):
+        c.append(td.text)
+    r = requests.get('https://restcountries.eu/rest/v2/name/'+c[1]+'?fullText=true')
+    r_data = r.json()
+    try:
+        flags.append(r_data[0]['flag'])
+    except Exception:
+        flags.append('none')
+        pass
+    countries_data.append(c)
+
+@app.route('/all')
 def covid():
 	return render_template('home.html')
 
@@ -17,7 +39,7 @@ def reslt():
 	return render_template('result.html',data=res.json()['cases_time_series'],l=len(res.json()['cases_time_series']))
 
 
-@app.route('/all')
+@app.route('')
 def all():
 	url="https://worldometers.info/coronavirus"
 	res=requests.get(url)
@@ -26,18 +48,10 @@ def all():
 	t_body = t_body_list[0]
 	tr_list=t_body.select('tr')
 	countries_data=[]
-	flags = []
 	for country in tr_list:
 		c=[]
 		for td in country.select('td'):
 			c.append(td.text)
-		r = requests.get('https://restcountries.eu/rest/v2/name/'+c[1]+'?fullText=true')
-		r_data = r.json()
-		try:
-			flags.append(r_data[0]['flag'])
-		except Exception:
-			flags.append('none')
-			pass
 		countries_data.append(c)
 	return render_template('all.html',data=countries_data,l=len(countries_data),flags=flags)
 
